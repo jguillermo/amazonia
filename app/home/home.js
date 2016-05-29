6b37913dd71851002afe3198af6a6fc30cfa4db9
Jose Guillermo <jguillermo@outlook.com>
2016-05-28 21:03:59 -0500

    home1

--- app/home/home.js
+++ app/home/home.js
@@ -3,12 +3,22 @@
 angular.module('myApp.home', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/home', {
-    templateUrl: 'home/home.html',
-    controller: 'HomeCtrl'
-  });
+	$routeProvider.when('/home', {
+		templateUrl: 'home/home.html',
+		controller: 'HomeCtrl'
+	});
 }])
 
-.controller('HomeCtrl', [function() {
-
+.controller('HomeCtrl', ['$scope', '$mdDialog', function($scope, $mdDialog) {
+	$scope.showModal = function(ev) {
+		$mdDialog.show(
+			$mdDialog.alert()
+			.parent(angular.element(document.querySelector('#popupContainer')))
+			.clickOutsideToClose(true)
+			.textContent('You can specify some description text in here.')
+			.ariaLabel('Alert Dialog Demo')
+			.ok('Got it!')
+			.targetEvent(ev)
+		);
+	}
 }]);
